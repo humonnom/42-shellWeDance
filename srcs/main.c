d84b1f5471977a208b3d34f28815f70bda6467d0
YeHam Kim <yamkim@YeHams-MacBook-Pro.local>
2021-02-01 12:44:49 +0900

    [main] first commit.

---
+++ srcs/main.c
@@ -0,0 +1,27 @@
+
+typedef struct  s_info
+{
+
+    int         exit
+    int         ret
+}               t_info;
+
+
+
+
+int main(int argc, char *argv[], char *env[]) {
+    t_info  info;
+
+    init_env();
+    inc_shell_level();
+
+    while(info->exit == ) {
+        init_sig();
+        
+
+    }
+
+    free()
+
+    return (info->ret)
+}
