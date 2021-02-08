5e0980c33c32a87ff52e05177f9931100832281d
YeHam Kim <yamkim@YeHams-MacBook-Pro.local>
2021-02-08 17:46:45 +0900

    [yekim] add algorithm for checking quote open and trimming cmd.

---
+++ srcs/test_yekim/playground.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("%d", 7 >> 2);
+
+	return (0);
+}
--- srcs/test_yekim/trim_cmd.c
+++ srcs/test_yekim/trim_cmd.c
@@ -1,41 +1,88 @@
-typedef struct		s_alist
+#include <stdlib.h>
+#include <stdio.h>
+extern int get_next_line(char **line);
+extern int check_quote_open(char *str);
+
+typedef struct		s_list
 {
 	void			*data;
-	struct s_alist	*next;
-}					t_alist;
+	struct s_list	*next;
+}					t_list;
 
-#include <stdio.h>
-static int	check_dquote_open(char *line)
+static int	is_quote(char c)
 {
-	// add single quote case
-	// ", ' check pair
-	int	open;
-	int	ret;
-
-	open = 0;
-	while (*line)
-	{
-		if (*line == '"')
-			++open;
-		++line;
-	}
-	printf("open: %d\n", open);
-	
+	if (c == '\"' || c == '\'')
+		return (1);
 	return (0);
 }
 
-#if 0
-static int trim_cmd(t_alist *arg_list)
+static int	get_cmd_size(char *str)
 {
-    //1. check dquote open and pair
-    //2. exclude dquote in arg_lst head
-    return (0);
+	int	ret;
+
+	ret = 0;
+	while (*str)
+	{
+		if (!is_quote(*str))
+			++ret;	
+		++str;
+	}
+	return (ret);
+}
+
+static char *copy_without_quote(char *str)
+{
+	char	*ret;
+	int		ret_size;
+	int		str_idx;
+	int		ret_idx;
+
+	ret_size = get_cmd_size(str);
+	printf("ret_size: %d\n", ret_size);
+	if (!(ret = (char *)malloc(sizeof(char) * (ret_size + 1))))
+		return (NULL);
+	ret[ret_size] = '\0';
+	str_idx = -1;
+	ret_idx = -1;
+	while (str[++str_idx])
+	{
+		if (!is_quote(str[str_idx]))
+			ret[++ret_idx] = str[str_idx];
+	}
+	//free(str);
+	return (ret);
+}
+
+#if 1
+static int	trim_cmd(t_list *arg_list)
+{
+	int	ret;
+
+	ret = 0;
+	printf("str: %s\n", (char *)(arg_list->data));
+    //1. check if s/dquote is open or pair
+	if ((ret = check_quote_open(arg_list->data)))
+		return (ret);
+    //2. exclude dquote or squote in arg_lst head
+	if (!(arg_list->data = copy_without_quote(arg_list->data)))
+		return (ret);
+	printf("modified head: %s\n", (char *)(arg_list->data));
+	
+    return (ret);
 }
 #endif
 
-int main(int argc, char **argv) {
-	printf("input: %s\n", argv[1]);
-	check_dquote_open(argv[1]);
+int main() {
+//	char *line;
+//	get_next_line(&line);
+	t_list	arg_list;
 
+	// "a's""fasd'f"
+	char *line = "\"e\'c\"\"\'ho\'\'\"";
+	arg_list.data = line;
+	arg_list.next = NULL;
 
+	printf("result: %d\n", trim_cmd(&arg_list));
+
+	return (0);
 }
