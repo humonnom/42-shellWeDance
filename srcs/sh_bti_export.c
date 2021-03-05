8cac0201cfe71649ec5134859374e592c3447f81
Yeham Kim <yekim@c5r2s7.42seoul.kr>
2021-03-05 19:01:45 +0900

    [master] add error case many times = using export cmd.

--- srcs/sh_bti_export.c
+++ srcs/sh_bti_export.c
@@ -2,24 +2,32 @@
 
 static int	sh_bti_export_err_case(t_env *env)
 {
-	int	ret;
+	if (env->val[0] == '=')
+	{
+		return (1);
+	}
 
-	ret = 0;
-
-	return (ret);
+	return (0);
 }
 
 int	sh_bti_export(char **args, t_list *env_list)
 {
 	t_list	*tmp_elist;
 	t_env	*tmp_env;
+	int		err_case;
 
 	if (!(tmp_env = get_env(args[0])))
 		return (1);
+	err_case = sh_bti_export_err_case(tmp_env);
 	if (del_quote(&(tmp_env->key)))
 		return (1);
 	if (del_quote(&(tmp_env->val)))
 		return (1);
+	if (err_case)
+	{
+		printf("zsh: %s not found\n", &(tmp_env->val[1]));
+		return (1);
+	}
 	printf("new env => key: %s, val: %s\n", tmp_env->key, tmp_env->val);
 	// deal with double equal case (ex)
 	tmp_elist = get_elist(env_list, tmp_env->key);
