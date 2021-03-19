92598dfc12b765dac48ca50811e91b18379e3c88
humonnom <josojoso@gmail.com>
2021-03-19 20:26:53 +0900

    [master] modified handle_fflush.c

--- srcs/handle_fflush.c
+++ srcs/handle_fflush.c
@@ -7,7 +7,6 @@ char	*handle_fflush(char *backup)
 	free(backup);
 	backup = NULL;
 	ret = ft_strjoin("", "");
-//	printf("[%c] -> g_fsh_buf:%d\n", buf[0], g_fsh_buf);
 	g_fsh_buf = 0; 
 	return (ret);
 }
