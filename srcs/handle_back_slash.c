92bd1924ccc81af8dd23bf9da2271b204a26ccbf
humonnom <josojoso@gmail.com>
2021-04-20 17:12:20 +0900

    [main] modify handle_back_slash.c to refactoring.

--- srcs/handle_back_slash.c
+++ srcs/handle_back_slash.c
@@ -1,20 +1,30 @@
 #include "../incs/minishell.h"
 
-static char
-	select_quote(char target)
+static int
+	get_back_slash_len(char *str, char c)
 {
-	char	single_quote;
-	char	double_quote;
+	int		idx;
+	int		len;
 
-	single_quote = '\'';
-	double_quote = '\"';
-	if (target == single_quote)
-		return(double_quote);
-	return (single_quote);
+	idx = -1;
+	len = 0;
+	while (str[++idx])
+	{
+		if (str[idx] == c && str[idx + 1] == '\0')
+			return (-1);
+		if (str[idx] == c && str[idx + 1] != '\0')
+		{
+			len += 3;
+			idx++;
+		}
+		else
+			len++;
+	}
+	return (len);
 }
 
 static void
-	handle_back_slash_in_filename(
+	remove_back_slash_in_filename(
 	char *str,
 	char *ret,
 	int	*idx,
@@ -39,70 +49,32 @@ static void
 	}
 }
 
-static int
-	get_back_slash_len(char *str, char c)
+static char
+	select_quote(char target)
 {
-	int		idx;
-	int		len;
+	char	single_quote;
+	char	double_quote;
 
-	idx = -1;
-	len = 0;
-	while (str[++idx])
-	{
-		if (str[idx] == c && str[idx + 1] == '\0')
-			return (-1);
-		if (str[idx] == c && str[idx + 1] != '\0')
-		{
-			len += 3;
-			idx++;
-		}
-		else
-			len++;
-	}
-	return (len);
+	single_quote = '\'';
+	double_quote = '\"';
+	if (target == single_quote)
+		return(double_quote);
+	return (single_quote);
 }
-#if 0
-static int
-	has_filename_as_args(char *str)
-{
-		if (ft_strncmp("rm", str, 2))
-			return (1);
-		if (ft_strncmp("rm -rf", str, 6))
-			return (1);
-		if (ft_strncmp("touch", str, 6))
-			return (1);
-		if (ft_strncmp("mkdir", str, 5))
-			return (1);
-		return (0);
-}
-#endif
+
 
 static void
-	cvt_back_slash_to_quote(char *str, char *ret, char c)
+	cvt_back_slash_to_quote(char *str, char *ret, int *idx, int *ret_idx)
 {
-	int		len;
-	int		idx;
-	int		ret_idx;
-	int		file_name_flag;
 	char	quote;
-	
-	idx = -1;
-	ret_idx = -1;
-	file_name_flag = 0;
-	while (str[++idx] && ++ret_idx < len)
-	{
-		if (is_bracket(str[idx]))
-			handle_back_slash_in_filename(str, ret, &idx, &ret_idx);		
-		if (str[idx] == c)
-		{
-			quote = select_quote(str[idx + 1]);
-			ret[ret_idx] = quote;
-			ret[++ret_idx] = str[++idx];
-			ret[++ret_idx] = quote;
-		}
-		else
-			ret[ret_idx] = str[idx];
-	}
+
+	quote = select_quote(str[*(idx + 1)]);
+	ret[*(ret_idx)] = quote;
+	++(*ret_idx);
+	++(*idx);
+	ret[*(ret_idx)] = str[*(idx)];
+	++(*ret_idx);
+	ret[*(ret_idx)] = quote;
 }
 
 char
@@ -112,12 +84,24 @@ char
 {
 	char	*ret;
 	int		len;
+	int		idx;
+	int		ret_idx;
 
 	if ((len = get_back_slash_len(str, c)) == -1)
 		return (NULL);
 	if (!(ret = (char *)malloc(sizeof(char) * (len + 1))))
 		return (NULL);
 	ret[len] = '\0';
-	cvt_back_slash_to_quote(str, ret, c);
+	idx = -1;
+	ret_idx = -1;
+	while (str[++idx] && ++ret_idx < len)
+	{
+		if (is_bracket(str[idx]))
+			remove_back_slash_in_filename(str, ret, &idx, &ret_idx);		
+		if (str[idx] == c)
+			cvt_back_slash_to_quote(str, ret, &idx, &ret_idx);
+		else
+			ret[ret_idx] = str[idx];
+	}
 	return (ret);
 }
